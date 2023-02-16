23ff55b9104fae8efdf8fbb5548e6b8d41a1fbf0
Teri Phillips <phillips.terianne@gmail.com>
2023-02-16 12:38:40 -0500

    created schema

---
+++ db/schema.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+
+USE employees_db;
+
+CREATE TABLE department (
+  id INT NOT NULL,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+ 
+CREATE TABLE role (
+    id INT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id),
+    PRIMARY KEY (id)
+); 
+
+CREATE TABLE employee (
+  id INT NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT,
+  FOREIGN KEY (role_id)
+  REFERENCES role(id),
+  PRIMARY KEY (id)
+); 
