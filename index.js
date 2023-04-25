d2d725d6aeaad93e125641c934b896d8bdcfb6f5
Teri Phillips <phillips.terianne@gmail.com>
2023-04-25 09:12:00 -0400

    added employee function

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 import inquirer from "inquirer"
 import console_table from "console.table"
 import fetch from "node-fetch"
-function allChoices() {
 
+function allChoices() {
     inquirer.prompt([
         {
             type: 'list',
@@ -14,7 +14,6 @@ function allChoices() {
                 'Update an Employee Role', 'Quit']
         }
     ]).then(answers => {
-        console.log(answers.choice);
         if (answers.choice == 'View all Departments') {
             getDepartments().then(renderData);
         }
@@ -36,22 +35,26 @@ function allChoices() {
             });
         }
         else if (answers.choice == 'Add a Role') {
-        
             getDepartments()
-            .then((sql) => renderDeptChoices(sql))
-            .then(({ titleArray, jsonData}) => roleQuestions(titleArray, jsonData));
+                .then((sql) => renderDeptChoices(sql))
+                .then(({ titleArray, jsonData }) => roleQuestions(titleArray, jsonData));
         }
         else if (answers.choice == 'Add an Employee') {
-            //    TO DO: create back end and front end to add an employee
+
+            getRolesandEmployees()
+                .then(({ roleArray, managerArray, data }) => employeeQuestions(roleArray, managerArray, data));
         }
         else if (answers.choice == 'Update an Employee Role') {
             //    TO DO: create back end and front end to update an employee 
+            getEmployees()
+                .then((sql) => renderManagerChoices(sql))
+                .then(({ managerArray, jsonData }) => updateRoleQuestions(managerArray, jsonData));
         }
         else if (answers.choice == 'Quit') {
             console.log("******Thank you for using Employee Tracker!!!******");
         }
-    }
-    );
+    })
+
 }
 function roleQuestions(titleArray, jsonData) {
     inquirer.prompt([
@@ -85,7 +88,57 @@ function roleQuestions(titleArray, jsonData) {
         addRole(title, salary, deptId);
     });
 }
+function employeeQuestions(titleArray, managerArray, data) {
 
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'fName',
+            message: 'What is the employees first name?',
+        },
+        {
+            type: 'input',
+            name: 'lName',
+            message: 'What is the employees last name?',
+        },
+        {
+            type: 'list',
+            name: 'role',
+            message: 'What is the employees role?',
+            choices: titleArray,
+        },
+        {
+            type: 'list',
+            name: 'manager',
+            message: 'Who is the employees manager?',
+            choices: managerArray,
+        },
+    ]).then(answers => {
+        let fName = answers.fName;
+        let lName = answers.lName;
+        let roleId = 0;
+        let managerId =0;
+console.log(answers.role)
+        for (let i = 0; i < data.result.length; i++) {
+            console.log(data.result[i].title);
+            if (data.result[i].title == answers.role) {
+               roleId = i + 1;
+            }
+        }
+        console.log(roleId)
+        console.log(answers.manager)
+        let result = answers.manager.split(" ");
+        let managerFName = result[1]
+        let managerLName =  result[2]
+        for (let i = 0; i < data.managerResult.length; i++) {
+            if (data.managerResult[i].first_name == managerFName && data.managerResult[i].last_name) {
+               managerId = data.managerResult[i].id;
+            }
+        }
+console.log(managerId)
+addEmployee(fName, lName, roleId, managerId)
+    });
+}
 const getDepartments = () =>
     fetch('http:localhost:3001/department', {
         method: 'GET',
@@ -108,6 +161,20 @@ const getEmployees = () =>
             'Content-Type': 'application/json',
         },
     });
+const getRolesandEmployees = () =>
+    // fetch('http:localhost:3001/rolesandemployees', {
+    //     method: 'GET',
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //     },
+
+    // });
+
+    fetch('http:localhost:3001/rolesandemployees')
+  .then(response => response.json())
+  .then(data => processData(data))
+  .catch(error => console.error(error));
+
 const renderData = async (sql) => {
     let jsonData = await sql.json();
     console.table(jsonData);
@@ -117,50 +184,120 @@ const renderData = async (sql) => {
 const renderDeptChoices = async (sql) => {
     let jsonData = await sql.json();
     let titleArray = [];
-    let deptId;
     for (let i = 0; i < jsonData.length; i++) {
         titleArray.push(jsonData[i].department);
-        
+
     }
     return { titleArray, jsonData };
 }
-const addDepartment = (name) => {
-    fetch(`http://localhost:3001/add_dept/${name}`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ name: name }),
-    })
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error('Failed to add department');
-            }
-            console.log('Department added successfully');
-            allChoices();
-        })
-        .catch((error) => {
-            console.error(error);
-        });
-};
-const addRole = (title, salary, deptId) => {
-    fetch(`http://localhost:3001/add_role/${title}/${salary}/${deptId}`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ title: title, salary: salary, department_id: deptId }),
-    })
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error('Failed to add role');
-            }
-            console.log('Role added successfully');
-            allChoices();
-        })
-        .catch((error) => {
-            console.error(error);
-        });
-};
+const renderRoleChoices = async (sql) => {
+    let jsonData = await sql.json();
+    let titleArray = [];
+    for (let i = 0; i < jsonData.length; i++) {
+        titleArray.push(jsonData[i].title);
 
-allChoices();
+    }
+    return { titleArray, jsonData };
+}
+function processData(data) {
+    let roleArray = [];
+    for (let i = 0; i < data.result.length; i++) {
+        roleArray.push(data.result[i].title);
+    }
+         let managerArray = []; 
+         let managerArray2 = [];
+ for (let i = 0; i < data.managerResult.length; i++) { 
+   let managerName = ` ${data.managerResult[i].first_name} ${data.managerResult[i].last_name}`;
+   managerArray2.push(managerName, data.managerResult[i].id );
+   managerArray.push(managerName);
+  }
+  console.log("line 204")
+  console.log(roleArray)
+    return { roleArray, managerArray, managerArray2, data };
+ }
+//     renderRoleChoices(sql)
+//     renderManagerChoices(sql2)
+//     return {titleArray, managerArray}
+
+//     let roleData = await sql.json();
+//     let employeeData = await sql2.json();
+//     let roleArray = [];
+//     let managerArray = []; // Declare managerArray outside of the for loop to be able to add elements to it
+//     for (let i = 0; i < roleData.length; i++) { // Use roleData instead of jsonData
+//       roleArray.push(roleData[i].title);
+//     }
+//     for (let i = 0; i < employeeData.length; i++) { // Use employeeData instead of jsonDataMgr
+//       let managerName = ` ${employeeData[i].first_name} ${employeeData[i].last_name}`;
+//       managerArray.push(managerName);
+//     }
+//     return { roleArray, managerArray }; // Move the return statement outside of the for loops
+//   }
+  
+
+    const renderManagerChoices = async (sql) => {
+        let jsonDataMgr = await sql.json();
+        let managerArray = [];
+        for (let i = 0; i < jsonDataMgr.length; i++) {
+            let managerName = ` ${jsonDataMgr[i].first_name} ${jsonDataMgr[i].last_name}`
+            managerArray.push(managerName);
+        }
+        return { managerArray };
+    }
+    const addDepartment = (name) => {
+        fetch(`http://localhost:3001/add_dept/${name}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ name: name }),
+        })
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error('Failed to add department');
+                }
+                console.log('Department added successfully');
+                allChoices();
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    };
+    const addRole = (title, salary, deptId) => {
+        fetch(`http://localhost:3001/add_role/${title}/${salary}/${deptId}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ title: title, salary: salary, department_id: deptId }),
+        })
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error('Failed to add role');
+                }
+                console.log('Role added successfully');
+                allChoices();
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    };
+    const addEmployee = (fName, lName, roleId, managerId) => {
+        fetch(`http://localhost:3001/add_employee/${fName}/${lName}/${roleId}/${managerId}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ first_name: fName, last_name: lName, role_id: roleId }),
+        })
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error('Failed to add employee');
+                }
+                console.log('Employee added successfully');
+                allChoices();
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    };
+    allChoices();
--- server.js
+++ server.js
@@ -38,6 +38,7 @@ let sql = 'SELECT title, salary, department FROM role JOIN department ON departm
     return sql;
   });
 });
+
 app.get('/employee', (req, res) => {
   let sql = 'SELECT e.first_name, e.last_name, r.title, d.department as department, r.salary, CONCAT(m.first_name, " ", m.last_name) as manager FROM employee e JOIN role r ON e.role_id = r.id JOIN department d ON r.department_id = d.id LEFT JOIN employee m ON e.manager_id = m.id';
   db.query(sql, (err, result) => {
@@ -46,6 +47,22 @@ app.get('/employee', (req, res) => {
     res.send(result);
   });
 });
+app.get('/rolesandemployees', (req, res) => {
+  let sql = 'SELECT title, salary, department FROM role JOIN department ON department.id = role.department_id';
+  let sql2 = 'SELECT first_name, last_name, id FROM employee';
+  
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+
+    db.query(sql2, (err, managerResult) => {
+      if (err) throw err;
+
+      res.send({ result, managerResult });
+    });
+  });
+});
+
+
 
 app.post('/add_dept/:name', (req, res) => {
   const name = req.params.name;
@@ -88,7 +105,21 @@ app.post('/add_dept/:name', (req, res) => {
     });
   });
 
-
+  app.post('/add_employee/:fName/:lName/:role_id/:manager_id', (req, res) => {
+    const fName = req.params.fName;
+    const lName = req.params.lName;
+    const role_id = req.params.role_id;
+    const manager_id = req.params.manager_id;
+    const query = 'INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)';
+    db.query(query, [fName, lName, role_id, manager_id], (err, result) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Error adding employee');
+      } else {
+        res.send('Employee added successfully');
+      }
+    });
+  });
   // Default response for any other request (Not Found)
   app.use((req, res) => {
     res.status(404).end();
